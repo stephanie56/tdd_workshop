3b869b1779bcfa3a2244639f3da5b5c900bf1b57
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-04-19 23:10:12 -0400

    Removed case switch for verser

--- notes/starter/bottles/lib/bottles.rb
+++ notes/starter/bottles/lib/bottles.rb
@@ -16,6 +16,14 @@ class Bottles
     end
   end
 
+  def numofbottleUpper(number)
+    if number == 0
+      "No more"
+    else
+      number
+    end
+  end
+
   def bottle(number)
     if number == 1
       "bottle"
@@ -32,14 +40,16 @@ class Bottles
     end
   end
 
-  def verse(number)
-
-    case number
-      when 0
-        "No more #{bottle(number)} of beer on the wall, #{numofbottle(number)} #{bottle(number)} of beer.\nGo to the store and buy some more, 99 #{bottle(number)} of beer on the wall.\n"
-      else
-        "#{number} #{bottle(number)} of beer on the wall, #{numofbottle(number)} #{bottle(number)} of beer.\nTake #{takedown(number)} down and pass it around, #{numofbottle(number - 1)} #{bottle(number - 1)} of beer on the wall.\n"
+  def handleSentence(number)
+    if number == 0
+      "Go to the store and buy some more, 99 #{bottle(number)} of beer on the wall."
+    else
+      "Take #{takedown(number)} down and pass it around, #{numofbottle(number - 1)} #{bottle(number - 1)} of beer on the wall."
     end
+  end
+
+  def verse(number)
+    "#{numofbottleUpper(number)} #{bottle(number)} of beer on the wall, #{numofbottle(number)} #{bottle(number)} of beer.\n#{handleSentence(number)}\n"
   end
 
 end
